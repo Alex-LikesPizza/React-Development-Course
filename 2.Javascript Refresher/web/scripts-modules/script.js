5034fb7cbbca88d02848830a9d1881d7167f5ce8
Alex-LikesPizza <106477187+Alex-LikesPizza@users.noreply.github.com>
2023-10-26 07:25:23 +0300

    Delete 2.Javascript Refresher directory

--- 2.Javascript Refresher/web/scripts-modules/script.js
+++
@@ -1,5 +0,0 @@
-import { apiKey1 as key1, apiKey2 as key2 } from "./utils.mjs";
-import def from "./utils.mjs";
-// import * as util from "./utils.js";
-
-console.log("Imports:", key1, key2, def);
--- 2.Javascript Refresher/web/scripts-modules/utils.mjs
+++
@@ -1,7 +0,0 @@
-const apiKey1 = '123eqwe12311qeqwe';
-const apiKey2 = 'asd123asd';
-
-
-export default "1";
-
-export { apiKey1, apiKey2 };
