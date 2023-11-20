8d6e8412b0ade05446fc9217790c76c7624ab9b9
Alex-LikesPizza <alexandru13321@gmail.com>
2023-11-20 18:17:29 +0200

    minor tweaks, removed folder

--- 4.Styling with React/src/components/Header.jsx
+++ 4.Styling with React/src/components/Header.jsx
@@ -31,9 +31,7 @@ const StyledHeader = styled.header`
   }
   
   @media (min-width: 768px) {
-    & {
-      margin-bottom: 4rem;
-    }
+     margin-bottom: 4rem;
   
     & h1 {
       font-size: 2.25rem;
