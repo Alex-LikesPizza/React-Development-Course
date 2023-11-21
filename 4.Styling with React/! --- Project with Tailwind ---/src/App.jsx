9138340b8616b096298bb063d38e7603ac509a26
Alex-LikesPizza <alexandru13321@gmail.com>
2023-11-21 16:50:54 +0200

    TailwindCSS + React

---
+++ 4.Styling with React/! --- Project with Tailwind ---/src/App.jsx
@@ -0,0 +1,13 @@
+import AuthInputs from './components/AuthInputs.jsx';
+import Header from './components/Header.jsx';
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        {/* <AuthInputs /> This is n ot a Tailwind course. I may learn Tailwind at a later time*/}
+      </main>
+    </>
+  );
+}
